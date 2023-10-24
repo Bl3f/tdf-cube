a4b5eb59a52170dcd6e0027154d83d51e137a889
hi <hi@dataeng.news>
2023-10-24 20:03:40 +0000

    Use env variables for Preset sync.

--- cube.py
+++ cube.py
@@ -1,6 +1,7 @@
 # Cube configuration options: https://cube.dev/docs/config
 
 from cube import config
+import os
  
 @config('semantic_layer_sync')
 def semantic_layer_sync(ctx: dict) -> list[dict]:
@@ -11,8 +12,8 @@ def semantic_layer_sync(ctx: dict) -> list[dict]:
       'active': True,
       'config': {
         'database': "Cube Cloud: gcp-prod",
-        'api_token': "aa8571ad-66b4-4cf2-a7d1-13480b970ad2",
-        'api_secret': "215dea582d4506d220bccdcd912eeda571ba339b5193a82dc716c354c2f6d76d",
+        'api_token': os.getenv("PRESET_TOKEN"),
+        'api_secret': os.getenv("PRESET_SECRET"),
         'workspace_url': "aaad0f2c.us2a.app.preset.io"
       }
     }
