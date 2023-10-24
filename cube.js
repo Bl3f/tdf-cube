855713fdce9892f775790638cd849a2fef2a3d02
hi <hi@dataeng.news>
2023-10-24 19:44:10 +0000

    Add Semantic Layer sync to Preset

---
+++ cube.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semanticLayerSync: () => {
+    return [{
+      type: "preset",
+      name: "Preset Sync",
+      config: {
+        database: "Cube Cloud: gcp-prod",
+        api_token: "aa8571ad-66b4-4cf2-a7d1-13480b970ad2",
+        api_secret: "215dea582d4506d220bccdcd912eeda571ba339b5193a82dc716c354c2f6d76d",
+        workspace_url: "aaad0f2c.us2a.app.preset.io"
+      }
+    }];
+  }
+};
